cebff01d547a241bb49467f8418f67392901f339
Niek <git@niekvantoepassing.nl>
2019-04-04 20:42:16 +0200

    added top discoveries stats

--- models/stats.js
+++ models/stats.js
@@ -32,6 +32,36 @@ module.exports = {
         GROUP by S.artist_id, S.album_id
         ORDER by count(*) desc
         LIMIT ${offset},${limit}`);
-    }
+    },
+
+    getTopArtistDiscoveries: function(limit = 10, offset = 0) {
+        return database.executeQuery(`
+        SELECT A.name as artist, count(*) as scrobbles
+        FROM Scrobble as S
+        INNER JOIN Artist as A on A.id = S.artist_id
+        WHERE utc >= strftime('%s', datetime('now', '-180 day'))
+        AND s.artist_id not in (
+            select distinct(artist_id) from Scrobble where utc < strftime('%s', datetime('now', '-180 day'))
+        )
+        GROUP by s.artist_id
+        ORDER by count(*) desc
+        LIMIT ${offset},${limit}`);
+    },
+
+    getTopAlbumDiscoveries: function(limit = 10, offset = 0) {
+        return database.executeQuery(`
+        SELECT A.name as artist, B.name as album, count(*) as scrobbles
+        FROM Scrobble as S
+        INNER JOIN Artist as A on A.id = S.artist_id
+        INNER JOIN Album as B on B.id = S.album_id
+        WHERE utc >= strftime('%s', datetime('now', '-180 day'))
+        and (s.artist_id || '-' || s.album_id) not in (
+            select distinct(artist_id || '-' || album_id) as record from Scrobble where utc < strftime('%s', datetime('now', '-180 days'))
+        )
+        GROUP by s.artist_id, s.album_id
+        ORDER by count(*) desc
+        LIMIT ${offset},${limit}`);
+    },
+
 }
 
--- routes/stats.js
+++ routes/stats.js
@@ -5,7 +5,7 @@ var createError = require('http-errors');
 
 router.get('/', function (req, res, next) {
 	stats.getRecentTracks().then(function (tracks) {
-		res.render('stats/recent-tracks', { title: 'Recent tracks', tracks: tracks })
+		res.render('stats/recent-tracks', { menu: 'recent', title: 'Recent tracks', tracks: tracks })
 	}).catch(function (error) {
 		next(createError(500, error));
 	});
@@ -13,7 +13,7 @@ router.get('/', function (req, res, next) {
 
 router.get('/artists', function (req, res, next) {
 	stats.getTopArtists().then(function (tracks) {
-		res.render('stats/top-artists', { title: 'Top 10 artists', tracks: tracks })
+		res.render('stats/top-artists', { menu: 'top-artists', title: 'Top 10 artists', tracks: tracks })
 	}).catch(function (error) {
 		next(createError(500, error));
 	});
@@ -21,7 +21,23 @@ router.get('/artists', function (req, res, next) {
 
 router.get('/albums', function (req, res, next) {
 	stats.getTopAlbums().then(function (tracks) {
-		res.render('stats/top-albums', { title: 'Top 10 albums', tracks: tracks })
+		res.render('stats/top-albums', { menu: 'top-albums', title: 'Top 10 albums', tracks: tracks })
+	}).catch(function (error) {
+		next(createError(500, error));
+	});
+});
+
+router.get('/artist-discoveries', function (req, res, next) {
+	stats.getTopArtistDiscoveries().then(function (tracks) {
+		res.render('stats/top-artists', { menu: 'artist-discoveries', title: 'Top artist discoveries in the past 180 days', tracks: tracks })
+	}).catch(function (error) {
+		next(createError(500, error));
+	});
+});
+
+router.get('/album-discoveries', function (req, res, next) {
+	stats.getTopAlbumDiscoveries().then(function (tracks) {
+		res.render('stats/top-albums', { menu: 'album-discoveries', title: 'Top album discoveries in the past 180 days', tracks: tracks })
 	}).catch(function (error) {
 		next(createError(500, error));
 	});
