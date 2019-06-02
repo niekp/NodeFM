a94bd6a5dde19f86af24f039fbd41703645b2b5d
Niek <git@niekvantoepassing.nl>
2019-06-02 22:00:39 +0200

    make the currently playing artist clickable

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,7 @@
 (function($){
 
     $(document).ready(function () {
-        $("[data-search='artist']").on('click', function() {
+        $("[data-search='artist'], .artist").on('click', function() {
             $('.modal').remove();
             $.get('/library/artist/' + encodeURI(($(this).text())) + '?modal=1', function (data) {
                 $('body').append(data);
