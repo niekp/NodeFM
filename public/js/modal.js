fd38d732c72634699bedb28d2380ff24f2b5ef42
Niek <git@niekvantoepassing.nl>
2019-06-03 07:55:12 +0200

    register click event after new pageload

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,14 +1,26 @@
 (function($){
 
     $(document).ready(function () {
-        $("[data-search='artist'], .artist").on('click', function() {
-            $('.modal').remove();
-            $.get('/library/artist/' + encodeURI(($(this).text())) + '?modal=1', function (data) {
-                $('body').append(data);
-                $('.modal').modal('show')
-                $('body').trigger('modal:loaded');
-            });
-        });
+        $("[data-search='artist'], .artist").on('click', setClickEvent);
+
+        $(".table").on('page:loaded', setClickEvent);
+
     });
+
+    function setClickEvent() {
+        $("[data-search='artist'], .artist").unbind('click');
+        $("[data-search='artist'], .artist").on('click', function () {
+            loadModal(this);
+        });
+    }
+
+    function loadModal(element) {
+        $('.modal').remove();
+        $.get('/library/artist/' + encodeURI(($(element).text())) + '?modal=1', function (data) {
+            $('body').append(data);
+            $('.modal').modal('show')
+            $('body').trigger('modal:loaded');
+        });
+    }
     
 })(jQuery);
