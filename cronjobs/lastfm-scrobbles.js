a525760e2958e1affeaa1f2c95412a7e332e0eb3
Niek <git@niekvantoepassing.nl>
2019-06-11 08:09:05 +0200

    use graceful-fs to fix EMFILE errors

--- cronjobs/lastfm-scrobbles.js
+++ cronjobs/lastfm-scrobbles.js
@@ -1,7 +1,7 @@
 var database = require('../db.js')
 const sqlite3 = require('sqlite3');
 const config = require('config');
-const fs = require('fs');
+var fs = require('graceful-fs')
 const lastfm_helper = require('../models/lastfm_helper.js');
 
 let database_folder = config.get('database_folder');
@@ -12,22 +12,27 @@ if (database_folder.substr(0, database_folder - 1) !== '/') {
 
 module.exports = {
 	run: function () {
-		// Loop through all users
-		fs.readdir(database_folder, function (error, files) {
-			if (error) {
-				return console.error('Unable to scan users: ' + error);
-			}
+		try {
 
-			files.forEach(function (user_file) {
-				let username = '';
-				if (user_file.indexOf('.db') > 0) {
-					username = user_file.replace('.db', '');
+			// Loop through all users
+			fs.readdir(database_folder, function (error, files) {
+				if (error) {
+					return console.error('Unable to scan users: ' + error);
 				}
 
-				if (username) {
-					lastfm_helper.syncLastFm(username);
-				}
+				files.forEach(function (user_file) {
+					let username = '';
+					if (user_file.indexOf('.db') > 0) {
+						username = user_file.replace('.db', '');
+					}
+
+					if (username) {
+						lastfm_helper.syncLastFm(username);
+					}
+				});
 			});
-		});
+		} catch(ex) {
+			console.error('lastfm-scrobbles', ex);
+		}
 	},
 }
